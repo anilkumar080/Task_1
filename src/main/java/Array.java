8b1141f6f2543b34f71191c00745e9b0f553ef73
anilkumar080 <154390074+anilkumar080@users.noreply.github.com>
2024-01-04 21:44:15 +0530

    commit

---
+++ src/main/java/Array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int numbers ;
+        //Here we are iterating the given number
+        for (numbers = 10; numbers <=50; numbers++) {
+            System.out.println("The numbers are"+" "+ numbers);
+        }
+    }
+}
